949c3f17337e4668bc4003dafb29d95e57a9e1fc
John Souza <souzajohnlee@gmail.com>
2020-02-03 15:27:58 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,13 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp(nds)
 end
 
 def print_first_directors_movie_titles
+#  directors_database.each do |director|
+#    directors_database[director][:moveie_title]
+  #end
 end
+
+pretty_print_nds(directors_database)
